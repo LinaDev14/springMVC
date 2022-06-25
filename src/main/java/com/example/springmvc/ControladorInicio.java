96f5d6f6bd4f863fee46d59d2ccaf961e75a056c
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-06-25 14:24:06 -0500

    se comparte informacion desde aplication properties

--- src/main/java/com/example/springmvc/ControladorInicio.java
+++ src/main/java/com/example/springmvc/ControladorInicio.java
@@ -1,18 +1,26 @@
 package com.example.springmvc;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @Slf4j
 public class ControladorInicio {
 
+    @Value("${index.saludo}")
+    private String saludo;
+
     @GetMapping("/")
-    public String inicio() {
+    public String inicio(Model model) {
+
+        var mensaje = "Adios mundo con Thymeleaf";
+
         log.info("Ejecutando el controlador Spring mvc");
-        //log.debug("mas detalle del controlador");
+        model.addAttribute("mensaje", mensaje);
+        model.addAttribute("saludo", saludo);
         return "index";
     }
 }
